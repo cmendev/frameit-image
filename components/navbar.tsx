86cfd5fa1dc2a012a6dce26c5f711d8b07009ecb
AndresMGDev <cmendoza022002@gmail.com>
2025-02-21 01:36:40 -0500

    format

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,8 @@ export default function NavBar() {
 
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between items-center">
-        <Image src="/me.webp" width={44} height={44} alt="me icon" />
-        <a href="/" className="text-xl font-bold">ImageTools</a>
-      </nav>
+      <Image src="/me.webp" width={44} height={44} alt="me icon" />
+      <a href="/" className="text-xl font-bold">ImageTools</a>
+    </nav>
   );
 }
